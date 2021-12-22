6cfbb8fac7df7b9cffeac4799e30ce74c0a9526c
AbimbolaHodonu <abimbolahodonu@onyx.boisestate.edu>
2021-12-21 17:15:18 -0700

    Smash Shell Commit

---
+++ SmashShell/rules.d
@@ -0,0 +1,3 @@
+smash.o: smash.c smash.h history.h
+commands.o: commands.c smash.h history.h
+history.o: history.c history.h
---
+++ SmashShell/smash.h
@@ -0,0 +1,20 @@
+
+/* NAME Senami Hodonu
+   smash.h -- Defines function prototypes and symbols for command.c 
+*/
+
+
+// Define function prototypes implemented in command.c
+#ifndef SMASH_H
+#define SMASH_H
+//Place all the regular config.h definitions here
+void executeCommand(char* str);
+
+void executeExternalCommand();
+
+
+#endif /*SMASH_H*/
+
+
+
+
